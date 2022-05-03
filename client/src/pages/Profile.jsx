019086f145d4028261c022cf6866352a9b904a3d
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-03 17:40:08 +0200

    made profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,7 @@
+import mystyles from "./styles/profile.module.scss";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import location from "../assets/location.png";
 
 function Profile() {
   let params = useParams();
@@ -14,7 +16,73 @@ function Profile() {
       }
     );
   }, []);
-  return <div>{users && <h1>Hello {users.firstname}</h1>}</div>;
+  return (
+    <main className={mystyles.wrapper}>
+      {users ? (
+        <main className={mystyles.container}>
+          <section className={mystyles.profile}>
+            <div className={mystyles.name}>
+              <div className={mystyles.image}></div>
+              <div className={mystyles.nameAndLocation}>
+                <h3>{users.firstname + " " + users.lastname}</h3>
+                <div className={mystyles.location}>
+                  <img src={location} alt="" />
+                  <p>Manzini, eSwatini</p>
+                </div>
+              </div>
+            </div>
+            <div className={mystyles.buttons}>
+              <button>Resume</button>
+              <button>Send Email</button>
+            </div>
+          </section>
+          <section className={mystyles.details}>
+            <div className={mystyles.leftSection}>
+              <div className={mystyles.skills}>
+                <h4>Skills</h4>
+                <ul>
+                  <li>React js</li>
+                  <li>JavaScript</li>
+                  <li>Node js</li>
+                  <li>Python</li>
+                </ul>
+              </div>
+              <div className={mystyles.links}>
+                <h4>Links</h4>
+                <ul>
+                  <li>GitHub</li>
+                  <li>Twitter</li>
+                  <li>LinkedIn</li>
+                </ul>
+              </div>
+            </div>
+            <div className={mystyles.mainSection}>
+              <div className={mystyles.bio}>
+                <div className={mystyles.bioHearder}>
+                  <h4>Web Developer</h4>
+                  <p>Experience: 2 years</p>
+                </div>
+                <p>
+                  Nunc eu urna id at vestibulum quisque lobortis et. Commodo
+                  tempor blandit erat gravida fermentum pellentesque habitant
+                  eget. Massa, aliquam sem ullamcorper faucibus pharetra
+                  faucibus sit sit metus. Lectus sit tristique nulla ac
+                  malesuada molestie velit commodo. Tortor egestas tempor
+                  integer iaculis.
+                </p>
+              </div>
+              <div className={mystyles.projects}>
+                <h4>Projects</h4>
+                <div className={mystyles.projectCards}></div>
+              </div>
+            </div>
+          </section>
+        </main>
+      ) : (
+        <></>
+      )}
+    </main>
+  );
 }
 
 export default Profile;
