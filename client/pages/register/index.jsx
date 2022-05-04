d171ca10e1b67ad3f34e8c2ad71fd0cdf6d4ac5b
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-04 21:08:49 +0200

    finished register form

--- client/pages/register/index.jsx
+++ client/pages/register/index.jsx
@@ -1,6 +1,11 @@
 import regstyles from "../../styles/register.module.scss";
 import { useState, useEffect, useRef } from "react";
 import plus from "../../public/assets/icons/plus.png";
+import github from "../../public/assets/icons/github.png";
+import twitter from "../../public/assets/icons/twitter.png";
+import linkedin from "../../public/assets/icons/linkedin.png";
+import internet from "../../public/assets/icons/internet.png";
+import avater from "../../public/assets/icons/avatar.png";
 import Image from "next/image";
 
 function Register() {
@@ -254,6 +259,33 @@ function Register() {
               rows="4"
             ></textarea>
 
+            <button type="submit">Next</button>
+            {responseError ? <small>{responseError}</small> : null}
+          </div>
+        )}
+        {links && (
+          <div className={regstyles.links}>
+            <div className={regstyles.file}>
+              <Image src={avater} width={40} height={40} />
+              <label>Upload a profile pic</label>
+              <input type="file" />
+            </div>
+            <div className={regstyles.skillsfield}>
+              <Image src={github} width={30} height={30} />
+              <input placeholder="Add you GitHub Link" type="text" />
+            </div>
+            <div className={regstyles.skillsfield}>
+              <Image src={linkedin} width={30} height={30} />
+              <input placeholder="Add you LinkedIn Link" type="text" />
+            </div>
+            <div className={regstyles.skillsfield}>
+              <Image src={twitter} width={30} height={30} />
+              <input placeholder="Add you Twitter Link" type="text" />
+            </div>
+            <div className={regstyles.skillsfield}>
+              <Image src={internet} width={30} height={30} />
+              <input placeholder="Add you Website Link" type="text" />
+            </div>
             <button type="submit">Next</button>
             {responseError ? <small>{responseError}</small> : null}
           </div>
