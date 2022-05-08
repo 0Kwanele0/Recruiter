b9721a59e56dd95789cb3c2f656139be19f97327
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-08 18:26:55 +0200

    added option to select field on login

--- client/pages/login/index.jsx
+++ client/pages/login/index.jsx
@@ -1,13 +1,12 @@
 import styles from "../../styles/login.module.scss";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useForm } from "react-hook-form";
 import Link from "next/link";
-import {useRouter} from 'next/router'
+import { useRouter } from "next/router";
 
 function Login() {
+  const router = useRouter();
 
-  const router = useRouter()
-  
   const {
     register,
     handleSubmit,
@@ -16,24 +15,79 @@ function Login() {
 
   const [responseError, setRecponseError] = useState();
 
+  const [recruiterCheckbox, setRecruiterCheckbox] = useState(false);
+  const [developerCheckbox, setDeveloperCheckbox] = useState(false);
+  const [checkboxError, setCheckboxError] = useState(false);
+  const recruiterBox = useRef();
+  const developerBox = useRef();
+
+  function labelClicked(e) {
+    switch (e.target.innerText) {
+      case "Developer":
+        setDeveloperCheckbox(true);
+        setRecruiterCheckbox(false);
+        recruiterBox.current.style.backgroundColor = "white";
+        developerBox.current.style.backgroundColor = "#2fe032";
+        return;
+      case "Recruiter":
+        setRecruiterCheckbox(true);
+        setDeveloperCheckbox(false);
+        recruiterBox.current.style.backgroundColor = "#2fe032";
+        developerBox.current.style.backgroundColor = "white";
+        return;
+    }
+  }
+  function recboxClicked(e) {
+    setRecruiterCheckbox(true);
+    setDeveloperCheckbox(false);
+    recruiterBox.current.style.backgroundColor = "#2fe032";
+    developerBox.current.style.backgroundColor = "white";
+    return;
+  }
+  function devboxClicked(e) {
+    setDeveloperCheckbox(true);
+    setRecruiterCheckbox(false);
+    recruiterBox.current.style.backgroundColor = "white";
+    developerBox.current.style.backgroundColor = "#2fe032";
+    return;
+  }
+
   const onSubmit = (data) => {
-    fetch("http://localhost:3001/user/login", {
-      method: "POST",
-      headers: { "Content-Type": "Application/json" },
-      body: JSON.stringify(data),
-    }).then(async (data) => {
-      const response = await data.json();
-      if (data.status == 200) {
-        console.log(response);
-        localStorage.setItem('recruiter-x-auth-token', response.token)
-        router.push('/devs')
-        setRecponseError();
-      } else if (data.status == 401) {
-        setRecponseError(response.msg);
-      } else {
-        setRecponseError(response.msg);
-      }
-    });
+    if (developerCheckbox) {
+      fetch("http://localhost:3001/user/login", {
+        method: "POST",
+        headers: { "Content-Type": "Application/json" },
+        body: JSON.stringify(data),
+      }).then(async (data) => {
+        const response = await data.json();
+        if (data.status == 200) {
+          console.log(response);
+          localStorage.setItem("recruiter-x-auth-token", response.token);
+          router.push("/devs");
+          setRecponseError();
+        } else {
+          setRecponseError(response.msg);
+        }
+      });
+    } else if (recruiterCheckbox) {
+      fetch("http://localhost:3001/recruiter/login", {
+        method: "POST",
+        headers: { "Content-Type": "Application/json" },
+        body: JSON.stringify(data),
+      }).then(async (data) => {
+        const response = await data.json();
+        if (data.status == 200) {
+          console.log(response);
+          localStorage.setItem("recruiter-x-auth-token", response.token);
+          router.push("/devs");
+          setRecponseError();
+        } else {
+          setRecponseError(response.msg);
+        }
+      });
+    } else {
+      setCheckboxError(true);
+    }
   };
 
   return (
@@ -54,6 +108,28 @@ function Login() {
         {errors.password?.type === "required" && (
           <small>Password is required</small>
         )}
+        <div className={styles.recOrDev}>
+          <p>Are you a Develper or a recruiter?</p>
+          <div className={styles.checkboxContainer}>
+            <div className={styles.checkbox}>
+              <div
+                onClick={recboxClicked}
+                ref={recruiterBox}
+                className={styles.check}
+              ></div>
+              <p onClick={labelClicked}>Recruiter</p>
+            </div>
+            <div className={styles.checkbox}>
+              <div
+                onClick={devboxClicked}
+                ref={developerBox}
+                className={styles.check}
+              ></div>
+              <p onClick={labelClicked}>Developer</p>
+            </div>
+          </div>
+          {checkboxError ? <small>Please select one!</small> : null}
+        </div>
         <button type="submit">Login</button>
         {responseError ? <small>{responseError}</small> : null}
         <p className={styles.referParagraph}>
