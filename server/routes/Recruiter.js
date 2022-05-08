cf9fe49af4ac9d8186fa396f31a493adb6072454
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-08 09:51:58 +0200

    authorized recruiter routes

--- server/routes/Recruiter.js
+++ server/routes/Recruiter.js
@@ -71,7 +71,7 @@ recruiterRouter.post("/login", (req, res) => {
   }
 });
 
-recruiterRouter.get("/:id", (req, res) => {
+recruiterRouter.get("/:id",authorize, (req, res) => {
   try {
     RecruiterModel.findById(req.params.id, (err, user) => {
       if (!user) {
@@ -85,7 +85,7 @@ recruiterRouter.get("/:id", (req, res) => {
   }
 });
 
-recruiterRouter.delete("/:id", (req, res) => {
+recruiterRouter.delete("/:id",authorize, (req, res) => {
   try {
     RecruiterModel.findByIdAndDelete(req.params.id, (err, done) => {
       if (err) {
@@ -100,7 +100,7 @@ recruiterRouter.delete("/:id", (req, res) => {
   }
 });
 
-recruiterRouter.put("/:id", (req, res) => {
+recruiterRouter.put("/:id",authorize, (req, res) => {
   RecruiterModel.findByIdAndUpdate(req.params.id, {
     $set: {
       firstname: req.body.firstname,
