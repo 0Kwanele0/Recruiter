5970c906c4b089d629f8793badc3121d05662741
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-09 13:43:22 +0200

    saved all details to local storage

---
+++ client/pages/myprofile/[id].jsx
@@ -0,0 +1,137 @@
+import mystyles from "../../styles/profile.module.scss";
+import location from "../../public/assets/icons/location.png";
+import ProjectCard from "../../components/ProjectCard";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+function MyProfile() {
+  const [user, setUser] = useState();
+  const [emailLink, setEmailLink] = useState();
+  const [imgLink, setImgLink] = useState();
+
+  const [links, setLinks] = useState();
+  const router = useRouter();
+  async function fetchUser(token) {
+    return fetch(`http://localhost:3001/user/${router.query.id}`, {
+      method: "GET",
+      headers: {
+        "recruiter-x-auth-token":token
+        ,
+      },
+    }).then(async (user) => {
+      if (user.status === 200) {
+        const data = await user.json();
+        setUser(data);
+        setEmailLink(`mailto: ${data.email}`);
+        setImgLink(`/uploads/profilephotos/${data.profilephoto}`);
+        const links ="";
+        setLinks(links);
+      } else {
+        router.push("/login");
+      }
+    });
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem("recruiter-x-auth-token");
+    if (token) {
+      fetchUser(token);
+    } else {
+      router.push("/login");
+    }
+  }, []);
+
+  return (
+    <main className={mystyles.wrapper}>
+      {user ? (
+        <main className={mystyles.container}>
+          <section className={mystyles.MyProfile}>
+            <div className={mystyles.name}>
+              <div className={mystyles.image}>
+                {imgLink && (
+                  <Image
+                    src={imgLink}
+                    objectFit="cover"
+                    width={60}
+                    height={60}
+                    alt=""
+                  />
+                )}
+              </div>
+              <div className={mystyles.nameAndLocation}>
+                <h3>{user.firstname + " " + user.lastname}</h3>
+                <div className={mystyles.location}>
+                  <Image width={20} height={20} src={location} alt="" />
+                  <p>{user.city + ", " + user.country}</p>
+                </div>
+              </div>
+            </div>
+            <div className={mystyles.buttons}>
+              <button>Resume</button>
+              <a href={emailLink}>Send Email</a>
+            </div>
+          </section>
+          <section className={mystyles.details}>
+            <div className={mystyles.leftSection}>
+              <div className={mystyles.skills}>
+                <h4>Skills</h4>
+                <ul>
+                  {user.skills &&
+                    user.skills.length > 0 &&
+                    user.skills.map((item, index) => {
+                      return <li key={index}>{item}</li>;
+                    })}
+                </ul>
+              </div>
+              <div className={mystyles.links}>
+                <h4>Links</h4>
+                <ul>
+                  {links &&
+                    links.map((item, index) => {
+                      if (item.link.length > 0) {
+                        return (
+                          <a key={index} href={item.link} target="_blank">
+                            <li>{item.name}</li>
+                          </a>
+                        );
+                      }
+                    })}
+                </ul>
+              </div>
+            </div>
+            <div className={mystyles.mainSection}>
+              <div className={mystyles.bio}>
+                <div className={mystyles.bioHearder}>
+                  <h4>{user.category}</h4>
+                  <p>Experience: {user.experience ? user.experience : 0}</p>
+                </div>
+                <p>{user.bio}</p>
+              </div>
+              <div className={mystyles.projects}>
+                <h4>Projects</h4>
+                <div className={mystyles.projectCards}>
+                  {user.projects &&
+                    user.projects.map((item, index) => {
+                      return (
+                        <ProjectCard
+                          key={index}
+                          title={item.title}
+                          description={item.description}
+                          link={item.link}
+                        />
+                      );
+                    })}
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+      ) : (
+        <> .....</>
+      )}
+    </main>
+  );
+}
+
+export default MyProfile;
