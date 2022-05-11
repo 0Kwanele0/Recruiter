ee12ee442ff2ef8429540bd568c31c64df5f2134
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-11 22:33:15 +0200

    added experience section

--- client/pages/profile/[id].jsx
+++ client/pages/profile/[id].jsx
@@ -1,6 +1,8 @@
 import mystyles from "../../styles/profile.module.scss";
 import location from "../../public/assets/icons/location.png";
 import pen from "../../public/assets/icons/pen.png";
+import plus from "../../public/assets/icons/plus.png";
+
 import close from "../../public/assets/icons/close.png";
 import ProjectCard from "../../components/ProjectCard";
 import Image from "next/image";
@@ -15,14 +17,44 @@ function Profile() {
   const [user, setUser] = useState();
   const [emailLink, setEmailLink] = useState();
   const [imgLink, setImgLink] = useState();
-  const [links, setLinks] = useState();
+  const [links, setLinks] = useState([]);
   const profileEditor = useRef();
   const [editPersonalDetails, setEditPersonalDetails] = useState(true);
   const [editLinks, setEditLinks] = useState(false);
   const [editSkills, setEditSkills] = useState(false);
   const [skills, setSkills] = useState([]);
   const [typedSkill, setTypedSkill] = useState("");
+  const [githubLink, setGithubLink] = useState("");
+  const [twitterLink, setTwitterLink] = useState("");
+  const [linkedinLink, setLinkedinLink] = useState("");
+  const [portfolioLink, setPortfolioLink] = useState("");
   const router = useRouter();
+  const experience = useRef();
+  const fieldList = useRef();
+  const [fieldlist, setFieldList] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState();
+  const [selectedExperience, setSelectedExperience] = useState();
+  const [exlist, setExList] = useState(false);
+
+  const listedCategories = [
+    "Web developer",
+    "Mobile developer",
+    "Frontend developer",
+    "Backend developer",
+    "Fullstack developer",
+    "Game developer",
+    "DevOps developer",
+    "Security developer",
+    "Data science developer",
+    "DevOps developer",
+  ];
+  const experienceList = [
+    "1 Year",
+    "2 Years",
+    "3 Years",
+    "4 Years",
+    "+5 Years",
+  ];
 
   async function fetchUser(token) {
     return fetch(`http://localhost:3001/user/${router.query.id}`, {
@@ -37,8 +69,13 @@ function Profile() {
         setEmailLink(`mailto: ${data.email}`);
         setImgLink(`/uploads/profilephotos/${data.profilephoto}`);
         setSkills(data.skills);
-        const links = "";
-        setLinks(links);
+        setLinks(JSON.parse(data.links[0]));
+        if (links.length > 0) {
+          setGithubLink(links[0].link);
+          setTwitterLink(links[1].link);
+          setLinkedinLink(links[2].link);
+          setPortfolioLink(links[3].link);
+        }
       } else {
         router.push("/login");
       }
@@ -64,12 +101,69 @@ function Profile() {
     );
   }
 
+  function showFieldList() {
+    setFieldList(!fieldlist);
+  }
+
+  function addCategory(ev) {
+    setSelectedCategory(ev.target.innerText);
+    showFieldList();
+  }
+  function addExperience(ev) {
+    setSelectedExperience(ev.target.innerText);
+    showExList();
+  }
+  function showExList() {
+    setExList(!exlist);
+  }
+
   function saveSkills(e) {
     e.preventDefault();
     fetch("", {
       method: "POST",
       headers: { "Content-Type": "Application/json" },
       body: JSON.stringify(skills),
+    }).then(async (response) => {
+      const data = await response.json();
+      if (response.status == 200) {
+        console.log(data);
+      } else {
+        console.log(data);
+      }
+    });
+  }
+
+  function changeLinks(e) {
+    switch (e.target.name) {
+      case "github":
+        setGithubLink(e.target.value);
+        return;
+      case "twitter":
+        setTwitterLink(e.target.value);
+        return;
+      case "linkedin":
+        setLinkedinLink(e.target.value);
+        return;
+      case "portfolio":
+        setPortfolioLink(e.target.value);
+        return;
+    }
+  }
+
+  function saveLinks(e) {
+    e.preventDefault();
+
+    const links = [
+      { name: "GitHub", link: githubLink },
+      { name: "Twitter", link: twitterLink },
+      { name: "LinkeIn", link: linkedinLink },
+      { name: "Portfolio", link: portfolioLink },
+    ];
+
+    fetch("", {
+      method: "POST",
+      headers: { "Content-Type": "Application/json" },
+      body: JSON.stringify(links),
     }).then(async (response) => {
       const data = await response.json();
       if (response.status == 200) {
@@ -135,8 +229,8 @@ function Profile() {
             </div>
             <div className={mystyles.menu}>
               <p onClick={changeEditMenu}>Personal details</p>
-              <p onClick={changeEditMenu}>Links</p>
               <p onClick={changeEditMenu}>Skills</p>
+              <p onClick={changeEditMenu}>Links</p>
             </div>
             <>
               {editPersonalDetails && (
@@ -180,25 +274,52 @@ function Profile() {
                   <button>Save</button>
                 </form>
               )}
-              {editLinks && (
+              {editSkills && (
                 <form action="">
                   <div className={mystyles.addSkills}>
-                    <input
-                      placeholder="Type you skill e.g React"
-                      type="text"
-                      value={typedSkill}
-                      onChange={(e) => {
-                        setTypedSkill(e.target.value);
-                      }}
-                      name=""
-                      id=""
-                    />
-                    <button
-                      onClick={addSkill}
-                      className={mystyles.addSkillsBtn}
-                    >
-                      Add
-                    </button>
+                    <div onClick={showExList} className={mystyles.inner}>
+                      {selectedExperience ? (
+                        <p className={mystyles.selectedExperience}>
+                          {selectedExperience}
+                        </p>
+                      ) : (
+                        <p>
+                          Experience <span>e.g 3 Years..</span>
+                        </p>
+                      )}
+                      <Image src={plus} width={20} height={20} />
+                    </div>
+                    {exlist && (
+                      <div ref={experience} className={mystyles.list}>
+                        <ul>
+                          {experienceList.map((item, index) => {
+                            return (
+                              <li key={index} onClick={addExperience}>
+                                {item}
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </div>
+                    )}
+                    <div className={mystyles.skillInput}>
+                      <input
+                        placeholder="Type you skill e.g React"
+                        type="text"
+                        value={typedSkill}
+                        onChange={(e) => {
+                          setTypedSkill(e.target.value);
+                        }}
+                        name=""
+                        id=""
+                      />
+                      <button
+                        onClick={addSkill}
+                        className={mystyles.addSkillsBtn}
+                      >
+                        Add
+                      </button>
+                    </div>
                   </div>
                   <div className={mystyles.displaySkills}>
                     {skills &&
@@ -214,11 +335,13 @@ function Profile() {
                   <button>Save</button>
                 </form>
               )}
-              {editSkills && (
+              {editLinks && (
                 <form action="">
                   <div className={mystyles.skillsfield}>
                     <Image src={github} width={40} height={40} />
                     <input
+                      value={githubLink}
+                      onChange={changeLinks}
                       name="github"
                       placeholder="Add you GitHub Link"
                       type="text"
@@ -227,6 +350,8 @@ function Profile() {
                   <div className={mystyles.skillsfield}>
                     <Image src={linkedin} width={40} height={40} />
                     <input
+                      value={linkedinLink}
+                      onChange={changeLinks}
                       name="linkedin"
                       placeholder="Add you LinkedIn Link"
                       type="text"
@@ -235,6 +360,8 @@ function Profile() {
                   <div className={mystyles.skillsfield}>
                     <Image src={twitter} width={40} height={40} />
                     <input
+                      value={twitterLink}
+                      onChange={changeLinks}
                       name="twitter"
                       placeholder="Add you Twitter Link"
                       type="text"
@@ -243,6 +370,8 @@ function Profile() {
                   <div className={mystyles.skillsfield}>
                     <Image src={internet} width={40} height={40} />
                     <input
+                      value={portfolioLink}
+                      onChange={changeLinks}
                       name="portfolio"
                       placeholder="Add your Portfolio Link"
                       type="text"
