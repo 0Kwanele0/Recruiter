1da57cba2a2eb3eff27efbf154a0cd6fc76dced4
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-18 23:42:01 +0200

    SASAS

--- server/index.js
+++ server/index.js
@@ -10,6 +10,15 @@ App.use(express.json());
 
 App.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 mongoose.connect(process.env.MONGO_URL, {}, (err) => {
   if (!err) {
     console.log("Connected to DataBase");
