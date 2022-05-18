5cabde370e7bdc63ce18c06dd908b6c5cd0c96d0
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-19 00:17:19 +0200

    sasas

--- server/index.js
+++ server/index.js
@@ -8,14 +8,7 @@ dotenv.config({ path: "./vars/.env" });
 const App = express();
 App.use(express.json());
 
-App.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
+App.use(cors());
 
 mongoose.connect(process.env.MONGO_URL, {}, (err) => {
   if (!err) {
