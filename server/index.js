878906690c70e3fcb089592d317923232bdb5e42
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-18 23:27:25 +0200

    sas

--- server/index.js
+++ server/index.js
@@ -8,9 +8,7 @@ dotenv.config({ path: "./vars/.env" });
 const App = express();
 App.use(express.json());
 
-App.use(
-  cors({ origin: "https://recruiteryfrxk-7gtnqd31g-0kwanele0.vercel.app" })
-);
+App.use(cors());
 
 mongoose.connect(process.env.MONGO_URL, {}, (err) => {
   if (!err) {
