04dbd1660dbb5f12008b02130eb46fb3fd854173
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-18 23:43:39 +0200

    DS

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ App.use(express.json());
 
 App.use(cors());
 
-app.use(function (req, res, next) {
+App.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
