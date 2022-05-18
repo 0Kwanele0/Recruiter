5020a3b55b1ec12ade9eb712301ce7bf9d7a52f5
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-18 23:56:53 +0200

    sasas

--- server/index.js
+++ server/index.js
@@ -8,15 +8,7 @@ dotenv.config({ path: "./vars/.env" });
 const App = express();
 App.use(express.json());
 
-App.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-App.use(cors());
+App.use(cors({ origin: "https://devrecruiter.vercel.app", credentials: true }));
 
 mongoose.connect(process.env.MONGO_URL, {}, (err) => {
   if (!err) {
