e763bc200f9bf2972abd8230cddb105c93c3d836
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-23 09:27:31 +0200

    added file delete

--- client/components/EditProfile/developer/DeleteAccount.jsx
+++ client/components/EditProfile/developer/DeleteAccount.jsx
@@ -2,27 +2,36 @@ import { useRouter } from "next/router";
 import styles from "../styles/editProfile.module.scss";
 
 function DeleteAccount(props) {
-  const router = useRouter();
+ const router = useRouter();
 
-  function Delete(e) {
-    e.preventDefault();
-    fetch(`${process.env.SERVER}/user/${props.user._id}`, {
-      method: "DELETE",
-      headers: {
-        "recruiter-x-auth-token": props.token,
-      },
-    }).then(async (response) => {
-      localStorage.removeItem("recruiter-x-auth-token");
-      router.reload();
-      router.push("/");
-    });
-  }
+ async function Delete(e) {
+  e.preventDefault();
+  fetch(`${process.env.SERVER}/user/${props.user._id}`, {
+   method: "DELETE",
+   headers: {
+    "recruiter-x-auth-token": props.token,
+   },
+  }).then(async (response) => {
+   const { data, error } = await supabase.storage
+    .from("main")
+    .remove([
+     `${props.user.profilephoto ? props.user.profilephoto : ""}`,
+     `${props.user.myresume ? props.user.myresume : ""}`,
+    ]);
+   if (data) {
+    localStorage.removeItem("recruiter-x-auth-token");
+    router.reload();
+   }
+   localStorage.removeItem("recruiter-x-auth-token");
+   router.reload();
+  });
+ }
 
-  return (
-    <form className={styles.form} action="submit" onSubmit={Delete}>
-      <button type="submit">Terminate Account</button>
-    </form>
-  );
+ return (
+  <form className={styles.form} action="submit" onSubmit={Delete}>
+   <button type="submit">Terminate Account</button>
+  </form>
+ );
 }
 
 export default DeleteAccount;
