2bdd90348002d505502c025730fb4bcc72a087d4
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-28 11:50:22 +0200

    added a loading indicator

--- client/pages/layout.js
+++ client/pages/layout.js
@@ -1,11 +1,26 @@
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import styles from "../styles/layout.module.scss";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
 
 export default function Layout({ children }) {
+  const loarder = useRef();
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      loarder.current.style.display = "block";
+    });
+    router.events.on("routeChangeComplete", () => {
+      loarder.current.style.display = "none";
+    });
+  }, [router]);
+
   return (
     <>
       <NavBar />
+      <div ref={loarder} className={styles.loader}></div>
       <main className={styles.body}>{children}</main>
       <Footer />
     </>
